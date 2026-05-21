de2bc45d2b65a60a5a48f03d3ff5c22ecd7b1cc1
dcuyu <dcuyu@Danielin>
2026-05-21 11:06:59 -0600

    Interfaz principal y de carga

--- src/smartplayer/SmartPlayer.java
+++ src/smartplayer/SmartPlayer.java
@@ -1,9 +1,12 @@
 package smartplayer;
 
+import smartplayer.interfaz.InterfazPrincipal;
+
 public class SmartPlayer {
 
     public static void main(String[] args) {
-        
+        InterfazPrincipal interfaz = new InterfazPrincipal();
+        interfaz.setVisible(true);
     }
     
 }
